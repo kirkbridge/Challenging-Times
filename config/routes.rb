b66989bae0650198c132c11f82d1345cd8026524
kirkbridge <bkirkbridetaylor@gmail.com>
2017-10-29 00:49:34 -0500

    Added navigation menu at the top which links to home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/', to: 'pages#home'
+  get '/', to: 'pages#home', as: 'home'
 
-  get '/about', to: 'pages#about'
+  get '/about', to: 'pages#about', as: 'about'
 
   get 'pages/home'
 
