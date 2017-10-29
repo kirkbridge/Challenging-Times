ff4267c15a73298aaf9d5ab8950d1c2bd922cad1
kirkbridge <bkirkbridetaylor@gmail.com>
2017-10-29 01:01:35 -0500

    Removed lizard controller

--- app/helpers/lizards_helper.rb
+++
@@ -1,2 +0,0 @@
-module LizardsHelper
-end
--- test/controllers/lizards_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LizardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
