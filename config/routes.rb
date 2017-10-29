4921ed2c0790ce8870013a5f8dc265fd1e9fcea0
kirkbridge <bkirkbridetaylor@gmail.com>
2017-10-29 00:34:38 -0500

    configured routs for home and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get '/', to: 'pages#home'
+
+  get '/about', to: 'pages#about'
+
   get 'pages/home'
 
   get 'pages/about'
 
-  root 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
